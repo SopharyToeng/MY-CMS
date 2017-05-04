ebe180abc9cf58378b0d293ee7c8bf9c00c31114
SopharyMSH <sopharydev@gmail.com>
2017-05-04 15:26:45 +0700

    added footer and navbar

--- app/apps/themes/my_first_theme/assets/js/common.coffee
+++ app/apps/themes/my_first_theme/assets/js/common.coffee
@@ -1,4 +1,26 @@
 MYCMS.Common = do ->
   _init = ->
+    _navbarToggle()
+    _scrollToTop()
+
+  _navbarToggle = ->
+    $('ul.dropdown-menu.submenu-dropdown [data-toggle=dropdown]').on 'click', (e) ->
+      event.preventDefault()
+      event.stopPropagation()
+      $('ul.dropdown-menu.submenu-dropdown [data-toggle=dropdown]').parent().removeClass('open')
+      $(this).parent().addClass('open')
+
+  _scrollToTop = ->
+    $(window).scroll ->
+      if $(this).scrollTop() > 50
+        $('#back-to-top').fadeIn()
+      else
+        $('#back-to-top').fadeOut()
+
+    $('#back-to-top').click ->
+      $('#back-to-top').tooltip 'hide'
+      $('body,html').animate { scrollTop: 0 }, 800
+
+    $('#back-to-top').tooltip 'show'
 
   { init: _init }
--- app/apps/themes/my_first_theme/main_helper.rb
+++ app/apps/themes/my_first_theme/main_helper.rb
@@ -1,27 +1,41 @@
 module Themes::MyFirstTheme::MainHelper
   def self.included(klass)
-    # klass.helper_method [:my_helper_method] rescue "" # here your methods accessible from views
+    klass.helper_method [:ccca_get_nav_menu] rescue ""
   end
 
   def my_first_theme_settings(theme)
-    # callback to save custom values of fields added in my_theme/views/admin/settings.html.erb
   end
 
-  # callback called after theme installed
   def my_first_theme_on_install_theme(theme)
-    # # Sample Custom Field
-    # unless theme.get_field_groups.where(slug: "fields").any?
-    #   group = theme.add_field_group({name: "Main Settings", slug: "fields", description: ""})
-    #   group.add_field({"name"=>"Background color", "slug"=>"bg_color"},{field_key: "colorpicker"})
-    #   group.add_field({"name"=>"Links color", "slug"=>"links_color"},{field_key: "colorpicker"})
-    #   group.add_field({"name"=>"Background image", "slug"=>"bg"},{field_key: "image"})
-    # end
-
-    # # Sample Meta Value
-    # theme.set_meta("installed_at", Time.current.to_s) # save a custom value
+    mycms_add_customize_theme_setting(theme)
   end
 
-  # callback executed after theme uninstalled
   def my_first_theme_on_uninstall_theme(theme)
   end
+
+  def ccca_get_nav_menu(key = 'main_menu', class_name = "navigation")
+    option = {
+      menu_slug: key,
+      container_class: class_name,
+      container_id: 'main-menu-ul',
+      item_class_parent: 'dropdown',
+      sub_class:        'dropdown-menu submenu-dropdown',
+      callback_item: lambda do |args|
+        args[:link_attrs] = "data-title='#{args[:link][:name].parameterize}'"
+        if args[:has_children]
+          args[:settings][:after] = "<span class='dropdown-icon'><i class='fa fa-angle-down' aria-hidden='true'></i></span>"
+          args[:link_attrs] += "data-toggle='dropdown'"
+        end
+      end
+    }
+    draw_menu(option)
+  end
+
+  def mycms_add_customize_theme_setting(theme)
+      if theme.get_field_groups.where(slug: 'theme-setting').blank?
+        group = theme.add_field_group({name: 'Theme Setting', slug: 'theme-setting'})
+        group.add_field({ name: 'MYCMS Logo', slug: 'mycms-logo' }, { field_key: 'image' })
+        group.add_field({ name: 'Address', slug: 'address' }, { field_key: 'text_area', translate: true })
+      end
+    end
 end
