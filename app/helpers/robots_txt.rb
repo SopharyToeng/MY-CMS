510b9291d4ee60eed9b2056d153f111dc1c3d03f
SopharyMSH <sopharydev@gmail.com>
2017-05-04 16:25:16 +0700

    blog website from google robots

---
+++ app/helpers/robots_txt.rb
@@ -0,0 +1,22 @@
+class RobotsTxt
+  def self.call(env)
+    # start building a new response
+    response = Rack::Response.new
+
+    # set content type to plain txt file
+    response['Content-Type'] = 'text/plain'
+    # cache the response for one year, so that further requests won't hit
+    # the application
+    response['Cache-Control'] = 'public, max-age=31557600' # cache for 1 year
+
+    # if we're not in production env, set the content to disallow all robots
+    unless Rails.env.production?
+      # disallow access to the whole site (/) for all agents (*)
+      response.write "User-agent: *\nDisallow: /"
+    else
+      response.write "User-agent: *\nDisallow:\n"
+    end
+
+    response.finish
+  end
+end
